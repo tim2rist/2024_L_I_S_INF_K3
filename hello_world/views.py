31f229ac72336968768fccc17ae2a309adba1b27
vdi-terminal <vdi-terminal@EDU-WSB.local>
2024-04-03 09:11:44 +0200

    Konfiguracja LINT_4

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,7 +3,7 @@ from hello_world.formater import get_formatted
 from hello_world.formater import SUPPORTED, PLAIN
 from flask import request
 
-moje_imie = "Tim"
+moje_imie = "Timm"
 msg = "Hello World!"
 
 
